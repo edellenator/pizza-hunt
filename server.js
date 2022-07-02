6ff611c6058ab01302ab29a4b26296e7ded58dc1
Erik Dell <edellenator@gmail.com>
2022-07-02 11:14:06 -0700

    testing redeployment again

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt', {
   useNewUrlParser: true,
@@ -16,6 +15,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hun
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
+app.use(express.static('public'));
 app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
